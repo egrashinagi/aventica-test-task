4acae7fa2260d782a019c4ef82f31832e15b70fc
egrashina.gi <10Fev1989Gal>
2018-08-28 18:00:46 +0300

    исправлен конфиг webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './index.js',
-
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
