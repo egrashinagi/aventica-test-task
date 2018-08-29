9b1008fb776b675791d782a6d879eee7db49ad98
egrashina.gi <10Fev1989Gal>
2018-08-29 12:05:17 +0300

    Почищен излишний код

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -9,18 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js'
     },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
-                })
-            }]
-    },
     plugins: [
-        new ExtractTextPlugin('index.css'),
         new HtmlWebpackPlugin({
             template: './src/index.html'
         })
@@ -28,6 +16,6 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        port: 9090
+        port: 8000
     }
 };
